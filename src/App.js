357ab99b03aca0b0bbb85920f8a7ccb9a3f9e270
Oni-Nick <nicholas00105@gmail.com>
2021-02-16 08:18:54 +0900

    Done Header part coding before starting Sidebar coding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import RecommendedVideos from "./RecommendedVideos";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // BEM naming convention
+    <div className="app">
+      {/* Header */}
+      <Header/>
+      <div className="app_page">
+        <Sidebar/>
+        <RecommendedVideos/>
+         {/* SIde bar */}
+         {/* RecommendedVideos */}
+      </div>
+     
+     
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD8kdvmCIKQ4YptCUne-Ij5x1F1HfrEaE4",
+    authDomain: "clone-35c75.firebaseapp.com",
+    projectId: "clone-35c75",
+    storageBucket: "clone-35c75.appspot.com",
+    messagingSenderId: "738294382765",
+    appId: "1:738294382765:web:4005bc9de42dc1d4690624",
+    measurementId: "G-9QKF8HH6DR"
+  };
